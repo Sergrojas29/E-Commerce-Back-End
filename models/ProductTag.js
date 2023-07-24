915782a277dd058e18d1f08ccad2e8481a68efde
Sergio Rojas <sergrojas29@gmail.com>
2023-07-24 18:36:30 -0400

    "Corrected the Models and Seeds cont. with routes"

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,13 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    product_id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    tag_id:{
+      type: DataTypes.INTEGER
+    }
   },
   {
     sequelize,
